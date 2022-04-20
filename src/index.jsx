d96c873bbf951ecf7d9bfc8b651cd0bc11b623ab
ChangChi <sam54537@gmail.com>
2022-04-20 23:36:13 +0800

    feat: firebase setup and sign in page ui

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,44 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBw45n_EfBKMYn_hFeLVyHE94OiIpOI_IM",
+  authDomain: "ss-chatroom-d0987.firebaseapp.com",
+  projectId: "ss-chatroom-d0987",
+  storageBucket: "ss-chatroom-d0987.appspot.com",
+  messagingSenderId: "630977914470",
+  appId: "1:630977914470:web:c6ca9e88a2b5a0ed622fa7",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./styles/index.css";
 
-import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Login auth={auth} />} />
+        <Route path="/chatroom" element={<Home />} />
+        <Route path="/register" element={<Register auth={auth} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 root.render(<App />);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { CalendarIcon, ChartBarIcon, FolderIcon, HomeIcon, InboxIcon, UsersIcon } from "@heroicons/react/outline";
+
+const navigation = [
+  { name: "Home", href: "#", icon: HomeIcon, current: true },
+  { name: "Team", href: "#", icon: UsersIcon, current: false },
+  { name: "Projects", href: "#", icon: FolderIcon, current: false },
+  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
+  { name: "Documents", href: "#", icon: InboxIcon, current: false },
+  { name: "Reports", href: "#", icon: ChartBarIcon, current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Home = () => {
+  return (
+    <div>
+      <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+        <div className="flex-1 flex flex-col min-h-0 bg-gray-800">
+          <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+            <div className="flex items-center flex-shrink-0 px-4">
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                alt="Workflow"
+              />
+            </div>
+            <nav className="mt-5 flex-1 px-2 space-y-1">
+              {navigation.map(item => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                  )}>
+                  <item.icon
+                    className={classNames(
+                      item.current ? "text-gray-300" : "text-gray-400 group-hover:text-gray-300",
+                      "mr-3 flex-shrink-0 h-6 w-6"
+                    )}
+                    aria-hidden="true"
+                  />
+                  {item.name}
+                </a>
+              ))}
+            </nav>
+          </div>
+          <div className="flex-shrink-0 flex bg-gray-700 p-4">
+            <a href="#" className="flex-shrink-0 w-full group block">
+              <div className="flex items-center">
+                <div>
+                  <img
+                    className="inline-block h-9 w-9 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </div>
+                <div className="ml-3">
+                  <p className="text-sm font-medium text-white">Tom Cook</p>
+                  <p className="text-xs font-medium text-gray-300 group-hover:text-gray-200">View profile</p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="md:pl-64 flex flex-col flex-1">
+        <main className="flex-1">
+          <div className="py-6">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+            </div>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+              {/* Replace with your content */}
+              <div className="py-4">
+                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+              </div>
+              {/* /End replace */}
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
